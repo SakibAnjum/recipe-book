00515beab2f688a4ec24ed82534c885cbcc18f48
sakibAnjum <sakib.ask@gmail.com>
2018-03-11 20:32:57 +0600

    upload

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ActionSheetController, AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 @IonicPage()
@@ -13,13 +13,19 @@ export class EditRecipePage implements OnInit{
   selectOptions = ['Easy','Medium', 'Hard'];
   recipeForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private actCtrl: ActionSheetController,
+    private alrtCtrl: AlertController
+    ) {
   }
 
 
 
   ngOnInit(){
     this.mode = this.navParams.get('mode');
+    this.initForm();
   }
 
 
@@ -28,7 +34,64 @@ export class EditRecipePage implements OnInit{
       'title' : new FormControl(null, Validators.required),
       'description' : new FormControl(null, Validators.required),
       'difficulty' : new FormControl('Medium', Validators.required),
+      'ingredients' : new FormArray([])
     })
+  }
+
+  onSubmit(){
+    console.log("sfd",this.recipeForm)
+  }
+
+  onManage(){
+    const actSheet = this.actCtrl.create({
+      title : "what ddo you want to do?",
+      buttons:[
+        {
+          text: 'Add',
+          handler: () =>{
+
+          }
+        },
+        {
+          text: 'Remove All',
+          role: "destructive",
+          handler: () =>{
+
+          }
+        },
+        {
+          text: 'Cancel',
+          role: "cancel"
+        }
+      ]
+    })
+
+    private createNew(){
+      const NewIng = this.alrtCtrl.create({
+        titlee: "Add",
+        inputs:[
+          {
+            name: 'name',
+            placeholder: "Name"
+          }
+        ],
+
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          },
+          {
+            text: 'Add',
+            handler: data =>{
+              if(data.name== '' || data.name == null){
+
+              }
+            }
+          }
+        ]
+      })
+    }
   }
 
 
